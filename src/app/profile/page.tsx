3b4575422d37a3a372c5838185d54b30ee0f323d
garriagus <agustin_g08@hotmail.com>
2023-06-08 21:17:26 -0300

    MODIFY from @/app/api/auth/

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 import Header from "@/components/header.component";
-import { authOptions } from "@/lib/auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export default async function Profile() {
   const session = await getServerSession(authOptions);
